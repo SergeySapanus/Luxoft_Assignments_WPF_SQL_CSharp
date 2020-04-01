e2f088ce6ddda5dfc06041f9d9f7963e9b6bd805
Sergey Sapanus <s.sapanus@systteh.ru>
2020-04-01 18:03:06 +0300

    feat: add 1_CSharp_Assignment project

---
+++ 1_CSharp_assignment/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _1_CSharp_Assignment
+{
+    class Program
+    {
+        private const int COUNT_UNIQUE_WORDS_THRESHOLD = 10000000;
+        private const int LENGTH_WORD_THRESHOLD = 50;
+
+        private const string DATA_FILE_PATH = "data.txt";
+        private const string RESULTS_FILE_PATH = "results.txt";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var encoding = GetEncoding(DATA_FILE_PATH);
+                var sortedByWord = GetSortedDataFromFile(DATA_FILE_PATH, encoding, COUNT_UNIQUE_WORDS_THRESHOLD, LENGTH_WORD_THRESHOLD);
+
+                //for (var i = 0; i < sortedList.Count; i++)
+                //{
+                //    Console.WriteLine($"{sortedList.Keys[i]}:{sortedList.Values[i]}");
+                //}
+
+                //Console.WriteLine(new string('-', 10));
+
+                var sortedByNumber = QuickSortByNumber(sortedByWord);
+
+                //foreach (var pair in sortedArray)
+                //{
+                //    Console.WriteLine($"{pair.Key}:{pair.Value}");
+                //}
+
+                using var file = new StreamWriter(RESULTS_FILE_PATH, false, encoding);
+                foreach (var pair in sortedByNumber)
+                    file.WriteLine($"{pair.Key} {pair.Value}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static SortedList<string, int> GetSortedDataFromFile(string filePath, Encoding encoding,
+            int lengthWordThreshold, int countUniqueWordsThreshold)
+        {
+            var stringBuilder = new StringBuilder(lengthWordThreshold);
+            var sortedList = new SortedList<string, int>(countUniqueWordsThreshold);
+
+            using (var streamReader = new StreamReader(filePath, encoding))
+            {
+                int intSymbol;
+
+                while ((intSymbol = streamReader.Read()) >= 0 || stringBuilder.Length > 0)
+                {
+                    var charSymbol = (char)intSymbol;
+
+                    if (intSymbol == -1 || char.IsSeparator(charSymbol) || char.IsControl(charSymbol))
+                    {
+                        if (stringBuilder.Length == 0)
+                            continue;
+
+                        var key = stringBuilder.ToString();
+
+                        if (sortedList.TryGetValue(key, out var value))
+                        {
+                            sortedList[key] = ++value;
+                        }
+                        else
+                        {
+                            sortedList.Add(key, 1);
+                        }
+
+                        stringBuilder.Clear();
+                    }
+                    else
+                    {
+                        if (char.IsUpper(charSymbol))
+                            charSymbol = char.ToLower(charSymbol);
+
+                        stringBuilder.Append(charSymbol);
+                    }
+                }
+            }
+
+            return sortedList;
+        }
+
+        private static Encoding GetEncoding(string filename)
+        {
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76)
+                return Encoding.UTF7;
+
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf)
+                return Encoding.UTF8;
+
+            if (bom[0] == 0xff && bom[1] == 0xfe)
+                return Encoding.Unicode;
+
+            if (bom[0] == 0xfe && bom[1] == 0xff)
+                return Encoding.BigEndianUnicode;
+
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff)
+                return Encoding.UTF32;
+
+            return Encoding.ASCII;
+        }
+
+        private static IEnumerable<KeyValuePair<string, int>> QuickSortByNumber(IEnumerable<KeyValuePair<string, int>> sequence)
+        {
+            var words = sequence /*as KeyValuePair<string, int>[] ?? sequence.ToArray()*/;
+            if (words.Count() <= 1)
+                return words;
+
+            var pivot = words.First();
+            var less = words.Skip(1).Where(x => x.Value <= pivot.Value);
+            var greater = words.Skip(1).Where(x => x.Value > pivot.Value);
+
+            return QuickSortByNumber(greater).Union(new[] { pivot }).Union(QuickSortByNumber(less));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Luxoft_Assignments_WPF_SQL_CSharp
+# 1. C# assignment
+Write console application that reads text file `data.txt` and counts number of each unique word in it. Words should be compared using current culture, case-**in**sensitive.
+
+Results should be written to file `results.txt` (replaced if exists) where each line contains word in lower case followed by space and number of its occurrences in input file, sorted by that number and by word in alphabetical order (if number is the same).
+
+For example if file contents is `abc, def, abc and egh` then results should be as follows:
+
+> abc 2 <br> and 1 <br> def 1 <br> egh 1
+
+It is safe to assume that number of unique words in the file does not exceed **10 000 000** and each word is no longer than **50** characters.
+# 2. SQL assignment
+You need to develop schema for database that will store logistic data for small machine parts shop. It operates with:
+*	machine parts which have name;
+*	regions which have name;
+*	suppliers which have name;
+*	deliveries which are made to certain region by certain supplier on certain date and consists of several lines; each line specifies machine part, its price in current delivery in **USD** dollars and number of these parts in this delivery.
+	
+Write down your schema and write SQL queries for obtaining info:
+
+*	get list of suppliers’ names that made at least one delivery to region with name `A`
+*	get summary value for all parts supplied by supplier `X` during **January 2000**
+*	get number of deliveries with total cost more than **1000 USD** (total cost is sum of price times number of parts by all lines for this delivery), grouped by supplier (result table should have columns `Supplier name` and `Delivery number`)
+*	get all supplier names who had not made any deliveries to regions where supplier `X` has any delivery
+# 3. WPF assignment
+Develop a small WPF application with single window which has text box, list box and OK button labeled `Number` and `Primes up to X:`, where **X** is the square of the number entered into text box (when value in the text box is updated – label and list box’s items should be updated immediately).
+
+Text box’s background should be green if user entered valid positive integer and salmon otherwise.
+
+OK button should be enabled if and only if user entered positive integer into text box and selected one item in the list box. By clicking **OK** – message box should be shown stating user’s choice.
+
+You have to use **MVVM** pattern for this app.
+
+Consider this example:
+*	user enters `x` in the text box – text box’s background is **salmon**, list box is empty, **OK** button is disabled;
+*	user enters `4` in the text box – text box’s background is **green**, list box contains items `2`, `3`, `5`, `7`, `1`” and `13`, nothing is selected, **OK** button is disabled; then user selects `5` – **OK** button becomes enabled; when clicked – message box `You have chosen 4 and 5` appears.
